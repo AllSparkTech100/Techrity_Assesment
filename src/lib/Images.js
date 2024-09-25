94fd2b49fe912948cc7273303ae96b64f098df1f
Alexander Jr <ikangenyinabasiono@gmail.com>
2024-09-25 11:48:36 +0100

    Update Images.js

--- src/lib/Images.js
+++ src/lib/Images.js
@@ -1,6 +1,6 @@
 export { default as happy_child } from "../assets/happy.webp";
 export { default as headphone } from "../assets/headphone.webp";
-export { default as ipsum } from "../assets/logo.png";
+export { default as ipsum } from "../assets/Logo.png";
 export { default as Ipsum } from "../assets/ipsum.png";
 export { default as lorem } from "../assets/Logo (4).png";
 export { default as oldIpsum } from "../assets/Logo (3).png";
